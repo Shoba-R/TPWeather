e3a39b6ae8650c10ac053f9b26831fffe8131e32
Shoba <shobamayidd@gmail.com>
2022-12-04 16:21:32 -0300

    armando mis componentes

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import './App.css'
+import { Navbar } from "./components/Navbar/Navbar"
+import {Form} from "./components/Form/Form"
+
+function App() {
+
+
+  return (
+    <>
+    <div className="App">
+
+      <Navbar/>
+      <Form/>
+        
+    </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./Navbar.css"
+
+const Navbar = () => {
+    return(
+        <div className='container-navbar'>
+            <h3 className='titulo-navbar'></h3>
+        </div>
+    )
+}
+
+export { Navbar }
